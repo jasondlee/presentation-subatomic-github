9edc9e5964bd0447ac8961036198af181a9e5500
Jason Lee <jason@steeplesoft.com>
2024-01-25 22:29:44 -0600

    File cleanup

--- reveal/build.sh
+++ reveal/build.sh
@@ -1,7 +1,12 @@
+
+function build() {
+    npx asciidoctor-revealjs slides.ad
+}
+
+build
 inotifywait -e close_write,moved_to,create -m . |
 while read -r directory events filename; do
 #  if [ "$filename" = "index.adoc" ]; then
-    npx asciidoctor-revealjs slides.ad
-    echo Built
+    build
 #  fi
 done
